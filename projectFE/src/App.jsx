a8631cebbd33ba39a9e7ed13d0d83b887bde2f2c
Radu Petrachioiu <radunicolae14@yahoo.com>
2023-06-02 01:22:17 +0300

    updates

--- projectFE/src/App.jsx
+++ projectFE/src/App.jsx
@@ -1,11 +1,19 @@
 import HomePage from "./HomePage";
 import RoleList from "./RoleList";
+import Login from './Pages/Login';
+import { Route, Switch } from 'react-router-dom';
+
+
 
 function App() {
     return (
-        <div className="App">
-            <HomePage />
-            <RoleList />
+        <div className={App}>
+
+                <HomePage />
+                <RoleList />
+                {/*<Login/>*/}
+
+
         </div>
     );
 }
